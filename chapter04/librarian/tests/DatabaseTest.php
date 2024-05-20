11a7b466216bbbe040740856ca9d31ec75fe7d4c
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2024-05-20 09:21:59 -0300

    Add missing tests to DatabaseTest class and remove replicated methods from JsonTest class in chapter 4 #35

--- chapter04/librarian/tests/DatabaseTest.php
+++ chapter04/librarian/tests/DatabaseTest.php
@@ -12,7 +12,7 @@ class DatabaseTest extends TestCase
     {
         replaceConfigFileContent("'database' => 'librarian'","'database' => 'librarian_test'");
     }
-
+    // author tests	
     /**
      * @coversNothing
      */
@@ -88,7 +88,91 @@ class DatabaseTest extends TestCase
         $author = readAuthorInDatabaseByCode(2);
         $this->assertEmpty($author);
         truncateTable('authors');
-    }    
+    }
+    
+    // book tests
+    /**
+     * @coversNothing
+     */
+    #[CoversNothing()]
+    public function testSaveBookInDatabase()
+    {
+        saveAuthorInDatabase('Wells','Herbert','George');
+        $this->assertTrue(saveBookInDatabase('The Time Machine',1));
+        truncateTable('books');
+        truncateTable('authors');
+    }
+  
+    /**
+     * @coversNothing
+     */    
+    #[CoversNothing()]
+    public function testReadBookInDatabase()
+    {
+        saveAuthorInDatabase('Von Goethe','Wolfgang','Johann');
+        saveBookInDatabase('Fausto',1);
+        $book = readBookInDatabaseByCode(1);
+        $this->assertStringContainsString('Fausto',$book['title']);
+        truncateTable('books');
+        truncateTable('authors');
+    }
+    
+    /**
+     * @coversNothing
+     */    
+    #[CoversNothing()]
+    public function testReadBooksInDatabase()
+    {
+        saveAuthorInDatabase('Christie','Mary','Agatha');
+        saveBookInDatabase('Murder on the Orient Express',1);
+        saveBookInDatabase('Death on the Nile',1);
+        saveBookInDatabase('Halloween Party',1); 
+        $books = readBooksInDatabase();
+        $this->assertCount(3,$books);
+        $this->assertStringContainsString('Orient',$books[0]['title']);
+        truncateTable('books');
+        truncateTable('authors');
+    }
+    
+    /**
+     * @coversNothing
+     */    
+    #[CoversNothing()]
+    public function testUpdateBookInDatabase()
+    {
+        saveAuthorInDatabase('Balzac','de','Honoré');
+        saveBookInDatabase('La Vendetta',1);
+        saveBookInDatabase('Le Contrat de mariage',1);
+        saveBookInDatabase('La Femme de trente ans',1);
+        $book = readBookInDatabaseByCode(2);
+        $this->assertStringContainsString('mariage',$book['title']);
+        updateBookInDatabase(2,[
+            'title' => 'Un début dans la vie'
+        ]);
+        $book = readBookInDatabaseByCode(2);
+        $this->assertStringContainsString('vie',$book['title']);
+        truncateTable('books');
+        truncateTable('authors');
+    }
+   
+    /**
+     * @coversNothing
+     */    
+    #[CoversNothing()]
+    public function testDeleteBookInDatabase()
+    {
+        saveAuthorInDatabase('Alencar','de','José');
+        saveBookInDatabase('O Guarani',1);
+        saveBookInDatabase('Iracema',1);
+        saveBookInDatabase('Ubirajara',1);
+        $book = readBookInDatabaseByCode(2);
+        $this->assertStringContainsString('Iracema',$book['title']);
+        deleteBookInDatabase(2);
+        $book = readBookInDatabaseByCode(2);
+        $this->assertEmpty($book);
+        truncateTable('books');
+        truncateTable('authors');
+    }
 
     public static function tearDownAfterClass():void
     {
